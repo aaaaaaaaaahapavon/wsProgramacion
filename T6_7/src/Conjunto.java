b7be1a31081ee58f0d85133dec84accff710c6cb
Ángel Pavón <bc.apavon.1995@iespabloserrano.com>
2022-02-15 14:08:53 +0100

    15/02

--- T6_7/src/Conjunto.java
+++ T6_7/src/Conjunto.java
@@ -10,24 +10,28 @@ public class Conjunto {
 		}
 
 		public int [] getConjunto() {
-			return conjunto;
+			return conjunto;                           
 		}
 
-		public void setConjunto() {
-			int i = 0;
-			this.conjunto [i] = this.num;
-			i++;
+		public void setConjunto(int n) {
+			do {
+				if(esUnico()==true) {
+					this.conjunto [n] = this.num;
+				}
+				else if(esUnico()==false){
+					setNum();
+				}
+			}while(esUnico()==false);
 		}
-
+			
 		public int getNum() {
 			return num;
 		}
 
-		public void setNum(int num) {
+		public void setNum() {
 			this.num = (int) (Math.random()*100);;
 		}
-		public void mostrarElementos(){
-			int i = 0;
+		public void mostrarElementos(int i){
 			System.out.println(conjunto[i]);
 			
 		}
@@ -35,10 +39,10 @@ public class Conjunto {
 		public boolean esUnico() {
 			boolean found = false;
 			for(int n = 0; n<conjunto.length; n++) {
-				if (conjunto [n]==num) {
-					found = false;
+				if (this.conjunto [n]!= this.getNum()) {
+					found = true;
 				}
-				else found = true;
+				else found = false;
 			}
 			if(found == false) {
 				return false;
--- T6_7/src/Principal.java
+++ T6_7/src/Principal.java
@@ -5,15 +5,14 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		
 		Conjunto conjunto = new Conjunto();
 		for(int i = 0; i<100;i++) {
-			if(conjunto.esUnico()== true) {
-				conjunto.setNum(0);
-				conjunto.setConjunto();
-				conjunto.mostrarElementos();
-			}
-			else i--;
-	}
+				conjunto.setNum();
+				conjunto.setConjunto(i);
+		}
+		for(int i = 0; i<100; i++) {
+			
+				conjunto.mostrarElementos(i);
+		}
 	}
 }
